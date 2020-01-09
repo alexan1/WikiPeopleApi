a6492a3a5e52e8859512c959a9b1e2366496be2e
Alexandre Konioukhov <alexan1@gmail.com>
2020-01-08 22:30:14 -0500

    PeopleController

---
+++ WikiPeopleApi/Controllers/PeopleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WikiPeopleApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetDetail([FromRoute] int id)
+        {
+            return Ok();
+        }
+    }
+}
